9f534d62f93576c9957102c7abfedcb5f6e23659
Lars Petersen <info@lars-petersen.net>
2019-02-03 15:42:14 +0100

    Adds screen state tracking

--- src-internal/System/Terminal/Virtual.hs
+++ src-internal/System/Terminal/Virtual.hs
@@ -1,6 +1,7 @@
 module System.Terminal.Virtual where
 
 import           Control.Monad.STM
+import           Control.Concurrent.STM.TVar
 import qualified Data.ByteString   as BS
 import           Control.Monad.IO.Class
 
@@ -10,14 +11,14 @@ import           System.Terminal.Terminal
 data VirtualTerminal
     = VirtualTerminal
     { virtualSettings          :: VirtualTerminalSettings
-    , virtualCursor            :: (Row, Col)
-    , virtualScreen            :: [String]
+    , virtualCursor            :: TVar (Row, Col)
+    , virtualScreen            :: TVar [String]
     }
 
 data VirtualTerminalSettings
     = VirtualTerminalSettings
     { virtualType              :: BS.ByteString
-    , virtualWindowSize        :: (Rows,Cols)
+    , virtualWindowSize        :: STM (Rows,Cols)
     , virtualEvent             :: STM Event
     , virtualInterrupt         :: STM ()
     }
@@ -26,18 +27,20 @@ instance Terminal VirtualTerminal where
     termType              = virtualType      . virtualSettings
     termEvent             = virtualEvent     . virtualSettings
     termInterrupt         = virtualInterrupt . virtualSettings
-    termCommand           = undefined
+    termCommand t c       = atomically (command t c) 
     termFlush _           = pure ()
     termGetWindowSize     = pure . virtualWindowSize . virtualSettings
     termGetCursorPosition = pure . virtualCursor
 
 withVirtualTerminal :: (MonadIO m) => VirtualTerminalSettings -> (VirtualTerminal -> m a) -> m a
-withVirtualTerminal settings handler = handler term
+withVirtualTerminal settings handler = do
+    term <- VirtualTerminal settings
+        <$> newTVar (0,0)
+        <*> newTVar (replicate height (replicate width ' '))
+    handler term
     where
-        term = VirtualTerminal
-            { virtualSettings          = settings :: VirtualTerminalSettings
-            , virtualCursor            = (0,0)
-            , virtualScreen            = replicate height (replicate width ' ')
-            }
         height = fst (virtualWindowSize settings)
         width  = snd (virtualWindowSize settings)
+
+command :: VirtualTerminal -> Command -> STM ()
+command  = undefined
